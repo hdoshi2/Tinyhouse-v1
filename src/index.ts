924727510cfcc4cbf14b229a4471c0086fffd8f7
Hari.Doshi <hdoshi2@gmail.com>
2021-04-21 22:51:04 -0600

    ADD: GraphQL and test GraphQL queries

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import {ApolloServer} from "apollo-server-express";
+import {listings} from './listings';
+import {schema} from './graphql';
 
 const app = express();
 const port = 9000;
 
+const server = new ApolloServer({schema});
+server.applyMiddleware({app, path: "/api"})
+
+app.use(bodyParser.json());
+
 app.get('/', (_req, res) => res.send('Hello'));
+
+
+app.get("/listings", (_req, res) => {
+    return res.send(listings);
+})
+
+
+app.post('/delete-listing', (req, res) => {
+    const id: string = req.body.id;
+
+    for(let i = 0; i < listings.length; i++){
+        if(listings[i].id === id){
+            return listings.splice(i,1)
+        }
+    }
+
+    return res.send("failed to delete listing");
+});
 
 app.listen(port);
 
