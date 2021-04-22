71280535842b41406b20095fd16cfc95f825ff8a
Hari.Doshi <hdoshi2@gmail.com>
2021-04-21 23:21:48 -0600

    ADD GraphQL query for listings.

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,11 +1,28 @@
-import {GraphQLObjectType, GraphQLString, GraphQLSchema} from "graphql";
+import {GraphQLObjectType, GraphQLString, GraphQLID, GraphQLInt, GraphQLFloat, GraphQLNonNull, GraphQLList, GraphQLSchema} from "graphql";
+import {listings} from "./listings";
+const Listing = new GraphQLObjectType({
+    name: "Listing",
+    fields: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        title: { type: GraphQLNonNull(GraphQLString) },
+        image: { type: GraphQLNonNull(GraphQLString) },
+        address: { type: GraphQLNonNull(GraphQLString) },
+        price: { type: GraphQLNonNull(GraphQLInt) },
+        numOfGuests: { type: GraphQLNonNull(GraphQLInt) },
+        numOfBeds: { type: GraphQLNonNull(GraphQLInt) },
+        numOfBaths: { type: GraphQLNonNull(GraphQLInt) },
+        rating: { type: GraphQLNonNull(GraphQLFloat) }
+    }
+});
 
 const query = new GraphQLObjectType({
     name: "Query",
     fields: {
-        hello:{
-            type: GraphQLString,
-            resolve: () => "Hello from the query"
+        listings: {
+            type: GraphQLNonNull(GraphQLList(GraphQLNonNull(Listing))),
+            resolve: () => {
+                return listings;
+            }
         }
     }
 });
