2dd31245d05402f4bea3fd2a205257a7a259c187
Hari.Doshi <hdoshi2@gmail.com>
2021-04-24 14:55:11 -0600

    REMOVE REST api routes

--- src/index.ts
+++ src/index.ts
@@ -29,25 +29,3 @@ mount(express());
 
 
 
-// // app.use(bodyParser.json());
-
-// app.get('/', (_req, res) => res.send('Hello'));
-
-
-// app.get("/listings", (_req, res) => {
-//     return res.send(listings);
-// })
-
-
-// app.post('/delete-listing', (req, res) => {
-//     const id: string = req.body.id;
-
-//     for(let i = 0; i < listings.length; i++){
-//         if(listings[i].id === id){
-//             return listings.splice(i,1)
-//         }
-//     }
-
-//     return res.send("failed to delete listing");
-// });
-
