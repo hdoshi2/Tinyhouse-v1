3b1bf3f2700414678e9403294f5af75ac8c14892
Hari.Doshi <hdoshi2@gmail.com>
2021-04-24 14:52:34 -0600

    ADD mongo db connection

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,53 @@
-import bodyParser from 'body-parser';
-import express from 'express';
+import express, {Application} from 'express';
 import {ApolloServer} from "apollo-server-express";
-import {listings} from './listings';
 import {typeDefs, resolvers} from './graphql';
+import {connectDatabase} from './database';
 
-const app = express();
 const port = 9000;
 
-const server = new ApolloServer({typeDefs, resolvers});
-server.applyMiddleware({app, path: "/api"})
+const mount = async (app: Application) => {
+
+    const db = await connectDatabase();
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: () => ({db})
+    });
+
+    server.applyMiddleware({app, path: "/api"})
+
+    app.listen(port);
+
+    console.log(`[app] http://localhost:${port}`);
+
+    const listings = await db.listings.find({}).toArray();
+    console.log(listings);
+}
+
+mount(express());
 
 
 
-app.use(bodyParser.json());
+// // app.use(bodyParser.json());
 
-app.get('/', (_req, res) => res.send('Hello'));
-
-
-app.get("/listings", (_req, res) => {
-    return res.send(listings);
-})
+// app.get('/', (_req, res) => res.send('Hello'));
 
 
-app.post('/delete-listing', (req, res) => {
-    const id: string = req.body.id;
-
-    for(let i = 0; i < listings.length; i++){
-        if(listings[i].id === id){
-            return listings.splice(i,1)
-        }
-    }
-
-    return res.send("failed to delete listing");
-});
+// app.get("/listings", (_req, res) => {
+//     return res.send(listings);
+// })
 
 
+// app.post('/delete-listing', (req, res) => {
+//     const id: string = req.body.id;
 
-app.listen(port);
+//     for(let i = 0; i < listings.length; i++){
+//         if(listings[i].id === id){
+//             return listings.splice(i,1)
+//         }
+//     }
 
-console.log(`[app] http://localhost:${port}`);
+//     return res.send("failed to delete listing");
+// });
+
